320ead105de7eed1fcf616ae2562152a56eca681
Weverton Ferreira <contato.wevertonfelipe@gmail.com>
2021-11-01 15:30:49 -0300

    Added position class

---
+++ console-chess/Board/Position.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Board
+{
+    class Position
+    {
+        public int line { get; set; }
+        public int column { get; set; }
+
+        public Position(int line, int column)
+        {
+            this.line = line;
+            this.column = column;
+        }
+
+        public override string ToString()
+        {
+            return $"{line}, {column}";
+        }
+    }
+}
---
+++ console-chess/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Board;
+namespace console_chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position pos = new Position(4, 3);
+            Console.WriteLine(pos);
+        }
+    }
+}
